58f062426e3b6b477705a2374018cd4d0ecf0f04
fprtkdl <128780835+fprtkdl@users.noreply.github.com>
2024-03-21 21:34:29 +0900

    Add files via upload

---
+++ js/home_script.js
@@ -0,0 +1,26 @@
+const swiperBox = new Swiper('.swiperBox > .swiper', {
+  loop: false,
+  slidesPerView: 6,
+  spaceBetween: 30,
+  resistande: false,
+  touchRatio: 3,
+  freeMode: true,
+  slidesOffsetBefore: 150
+})
+
+new Swiper('.Bar > .swiper', {
+  autoplay: {
+    delay: 5000
+  },
+  loop: true,
+  slidesPerView: 1,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next'
+  }
+});
